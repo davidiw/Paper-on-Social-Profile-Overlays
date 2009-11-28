17219b79e13b3fedc391577e0dc9d7a04a4cced4
David Wolinsky <davidiw@ufl.edu>
2009-11-28 00:26:29 -0500

    Initial commit

---
+++ make.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+paper=iptps10
+latex $paper -interaction=nonstopmode
+bibtex $paper
+latex $paper -interaction=nonstopmode
+latex $paper -interaction=nonstopmode
+dvips -t letter $paper.dvi
+ps2pdf $paper.ps
+xpdf $paper.pdf
